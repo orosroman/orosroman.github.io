4d4102616ab385a3d592cab5d4732e78142ad5cc
Roman <oros.roman1@gmail.com>
2016-10-25 19:32:48 +0300

    New page is created

---
+++ js/script.js
@@ -0,0 +1,19 @@
+ $(document).ready(function(){
+	$("#menu, #scroll-up").on("click","a", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$("body,html").animate({scrollTop: top}, 1000);
+	});
+
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+		$(".scrollup").fadeIn();
+		} else {
+		$(".scrollup").fadeOut();
+		}
+	});
+
+	new WOW().init();
+});
+
